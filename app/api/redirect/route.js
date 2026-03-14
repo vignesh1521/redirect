ab7961f2662dba0efc74812d01b8ec032d00b1eb
Vignesh <wvignesh983@gmail.com>
2026-03-14 16:45:14 +0530

    get error

--- app/api/redirect/route.js
+++ app/api/redirect/route.js
@@ -1,6 +1,14 @@
 import { NextResponse } from "next/server";
 
 
+export async function GET() {
+  return new Response("Custom error occurred", {
+    status: 500,
+    headers: {
+      "Content-Type": "text/plain"
+    }
+  });
+}
 
 export async function HEAD() {
   return new Response("Custom error occurred", {
