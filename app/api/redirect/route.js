7c84df8bf4b474d6d74d81681c5e6e8431c97e43
Vignesh <wvignesh983@gmail.com>
2026-03-14 16:40:47 +0530

    custom error

--- app/api/redirect/route.js
+++ app/api/redirect/route.js
@@ -1,32 +1,12 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
-
-  const headers = new Headers();
-
-  headers.set("Location", "google as");
-  headers.set("X-Test", "pentest");
-  headers.set("X-Powered-By", "Vignesh");
-
-  return new Response(null, {
-    status: 302,
-    headers: headers
-  });
-
-}
 
 
 export async function HEAD() {
-
-  const headers = new Headers();
-
-  headers.set("Location", "google as");
-  headers.set("X-Test", "pentest");
-  headers.set("X-Powered-By", "Vignesh");
-
-  return new Response(null, {
-    status: 302,
-    headers: headers
+  return new Response("Custom error occurred", {
+    status: 500,
+    headers: {
+      "Content-Type": "text/plain"
+    }
   });
-
 }
